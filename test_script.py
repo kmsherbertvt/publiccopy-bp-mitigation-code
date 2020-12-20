a8f44e5c315024a859f6ab4904f0405dc8c9ff98
George Barron <george.samuel.barron@gmail.com>
2020-12-20 00:52:00 -0500

    Switching to using bags

--- test_script.py
+++ test_script.py
@@ -3,6 +3,7 @@ from math import comb
 from dask.distributed import Client, progress, wait
 from dask_jobqueue import PBSCluster
 import dask
+import dask.bag as db
 import pandas as pd
 import time
 
@@ -10,7 +11,8 @@ from simulator import (
     Array,
     pauli_str,
     get_gradient_fd,
-    spc_ansatz
+    spc_ansatz,
+    pauli_ansatz
 )
 
 def get_op(n: int) -> Array:
@@ -26,45 +28,46 @@ def get_op(n: int) -> Array:
 
 if __name__ == '__main__':
     np.random.seed(42)
-    max_num_qubits = 6
-    num_samples = 10
-    qubits_range = range(4, max_num_qubits+2)[0:max_num_qubits:2]
-
-    lazy_data = []
-
-    for n in qubits_range:
-        op = get_op(n)
-        for m in range(2, n):
-            ans = spc_ansatz(n, m)
-            num_pars = 2*(comb(n, m) - 1)
-            grads = []
-            for i in range(num_samples):
-                point = list(np.random.uniform(-np.pi, +np.pi, size=num_pars//2))
-                point = point + [0.0]*(num_pars//2)
-
-                grad = dask.delayed(get_gradient_fd)(ans, op, point)
-                grads.append(grad)
-            
-            grad_list = dask.delayed(np.concatenate)(grads).flatten().real
-
-            mean = np.mean(grad_list)
-            var = np.var(grad_list)
-
-            lazy_data.append({
-                'n': n,
-                'm': m,
-                'mean': mean,
-                'var': var
-            })
-
-            
-    cluster = PBSCluster()
-    cluster.scale(1)
+    cluster = PBSCluster(n_workers=24)
+    cluster.scale(n=48)
     client = Client(cluster)
     client.upload_file('simulator.py')
 
-    futures = dask.persist(*lazy_data)
-    results = dask.compute(*futures)
-    df = pd.DataFrame(results)
+    # Barren plateaus for MCPs
+    max_num_qubits = 8
+    qubits_range = range(4, max_num_qubits+2)[0:max_num_qubits:2]
+    num_samples = 20
+    #layers = list(range(10, 100, 10)) + list(range(100,1000,100))
+    layers = range(10, 100, 10)
+
+    inputs = []
+
+    for n in qubits_range:
+        for l in layers:
+            axes = [
+                np.random.choice([0, 1, 2, 3], size=n)
+                for _ in range(l)
+            ]
+            for i in range(num_samples):
+                point = np.random.uniform(-np.pi, +np.pi, size=l)
+                inputs.append({'n': n, 'axes': axes, 'point': point})
+    
+    input_bag = db.from_sequence(inputs)
+
+    def grad_comp(n, axes, point):
+        ans = pauli_ansatz(axes=axes)
+        op = get_op(n)
+        grad = get_gradient_fd(ans=ans, op=op, point=point)
+        return {
+            'n': n,
+            'l': len(axes),
+            'grad': grad.real
+        }
+
+    bag = input_bag.map(lambda kwargs: grad_comp(**kwargs))
+
+    df = bag.to_dataframe()
+    df = df.explode('grad')
+    
     time = time.time()
-    df.to_csv(f'bp_spc_{time}.csv')
+    df.to_csv(f'data/bp_spc_*.csv')
