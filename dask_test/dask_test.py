473e9f7a80be45ff30db3693fc0fe00f19f1679a
George Barron <george.samuel.barron@gmail.com>
2021-01-12 11:58:15 -0500

    Updated gitignore

--- dask_test/dask_test.py
+++ dask_test/dask_test.py
@@ -1,26 +1,26 @@
 import dask
+import dask.bag as db
 from dask.distributed import Client, progress
-from dask_jobqueue import PBSCluster
+from dask_jobqueue import SLURMCluster
 import time
 import numpy as np
 
 
-@dask.delayed
-def add_slow(x, y) -> int:
-    time.sleep(np.random.uniform())
-    return x+y
-
-
 if __name__ == '__main__':
-    cluster = PBSCluster()
-    cluster.scale(2)
+    cluster = SLURMCluster(
+        n_workers=10
+        )
+    cluster.scale(1)
+
+    with open('dask_job_script.sh', 'w') as f:
+        f.write(cluster.job_script())
+
     client = Client(cluster)
 
-    results = []
-    for i in range(100):
-        x = np.random.uniform()
-        y = np.random.uniform()
-        results.append(add_slow(x, y))
-    
-    mean = np.mean(results)
-    print(client.compute(mean))
+    b1 = db.from_sequence(range(1000))
+    b2 = b1.map(lambda x: x+1)
+
+    res = b2.mean()
+    print(res)
+
+    print(res.compute())
