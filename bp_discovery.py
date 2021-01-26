8d7e2d5770cff51b83ef926695158de1bdf47b76
George Barron <george.samuel.barron@gmail.com>
2021-01-26 13:02:26 -0500

    Adding logging

--- bp_discovery.py
+++ bp_discovery.py
@@ -1,12 +1,14 @@
 import numpy as np
 from math import comb
 from dask.distributed import Client, progress, wait
+from typing import List
 from dask_jobqueue import SLURMCluster
 from random import shuffle
 import dask
 import dask.bag as db
 import pandas as pd
 import time
+import logging
 
 from simulator import (
     Array,
@@ -14,6 +16,11 @@ from simulator import (
     get_gradient_fd,
     spc_ansatz,
     pauli_ansatz
+)
+
+logging.basicConfig(
+    filename='bp_discovery.log',
+    level=logging.INFO
 )
 
 def get_op(n: int) -> Array:
@@ -28,11 +35,13 @@ def get_op(n: int) -> Array:
 
 
 if __name__ == '__main__':
-    print(f'Starting cluster and client...')
+    logging.info('Starting dask cluster/client...')
     t_o = time.time()
     np.random.seed(42)
     cluster = SLURMCluster(extra=["--lifetime-stagger", "2m"])
     #cluster.adapt(minimum_jobs=3, maximum_jobs=20)
+
+    logging.info('Scaling cluster...')
     cluster.scale(jobs=20)
     client = Client(cluster)
     client.upload_file('simulator.py')
@@ -46,8 +55,7 @@ if __name__ == '__main__':
 
     inputs = []
 
-    print(f'Defining experiments...')
-
+    logging.info('Defining experiments...')
     for n in qubits_range:
         for l in layers:
             axes = [
@@ -57,26 +65,33 @@ if __name__ == '__main__':
             for i in range(num_samples):
                 point = np.random.uniform(-np.pi, +np.pi, size=l)
                 inputs.append({'n': n, 'axes': axes, 'point': point})
+    logging.info(f'Defined {len(inputs)} experiments')
 
     # Shuffle so jobs are more homogeneous
+    logging.info('Shuffling experiments...')
     shuffle(inputs)
-
-    print(f'There are {len(inputs)} experiments to run')
 
     def grad_comp(n, axes, point):
         ans = pauli_ansatz(axes=axes)
         op = get_op(n)
+        logging.info(f'Computing gradient on {n} qubits')
         grad = get_gradient_fd(ans=ans, op=op, point=point)
+        logging.info(f'Finished computing gradient on {n} qubits')
         return {
             'n': n,
             'l': len(axes),
             'grad': grad.real
         }
 
+    logging.info('Defining bags')
     input_bag = db.from_sequence(inputs)
     bag = input_bag.map(lambda kwargs: grad_comp(**kwargs))
+    logging.info('Defining dataframe')
     df = bag.to_dataframe()
     df = df.explode('grad')
     df['grad'] = df['grad'].astype(float)
+    logging.info('Defining pivot...')
     pivot = df.groupby(['l', 'n']).agg({'grad': ['mean', 'std']})
+    logging.info('Dumping to file...')
     pivot.to_csv('data/rand/bp_rand_sum_*.csv')
+    logging.info('Dumped to file...')
