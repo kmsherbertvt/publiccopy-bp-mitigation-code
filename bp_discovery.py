4af7cb4c7ff556f59464d2be4621b41e6ada131e
George Barron <george.samuel.barron@gmail.com>
2021-02-08 12:43:58 -0500

    Updates, MCP working, SPC fails

--- bp_discovery.py
+++ bp_discovery.py
@@ -53,7 +53,8 @@ def gen_random_circs(
             ]
             for i in range(num_samples):
                 point = np.random.uniform(-np.pi, +np.pi, size=l)
-                inputs.append({'n': n, 'axes': axes, 'point': point})
+                k = np.random.randint(0, l)
+                inputs.append({'n': n, 'axes': axes, 'point': point, 'k': k})
     logging.info(f'Defined {len(inputs)} experiments')
 
     # Shuffle so jobs are more homogeneous
@@ -89,7 +90,6 @@ def gen_mcp_circs(
 
     logging.info('Defining experiments for MCP circs')
 
-
     for n in qubits_range:
         pool = mcp_g_list(n)
         for l in depth_range:
@@ -97,19 +97,23 @@ def gen_mcp_circs(
                 point = np.random.uniform(-np.pi, +np.pi, size=l)
                 axes_inds = list(np.random.choice(range(len(pool)), size=l))
                 axes = [pool[i] for i in axes_inds]
-                inputs.append({'n': n, 'l': l, 'point': point, 'axes': axes})
+                k = np.random.randint(0, l)
+                inputs.append({'n': n, 'l': l, 'point': point, 'axes': axes, 'k': k})
     logging.info(f'Defined {len(inputs)} experiments')
+    shuffle(inputs)
 
-    def grad_comp(n, axes, point, l):
+    def grad_comp(n, axes, point, l, k):
         ans = pauli_ansatz(axes)
         op = get_op(n)
 
         logging.info(f'Computing gradient on {n} qubits')
-        grad = get_gradient_fd(ans=ans, op=op, point=point)
+        grad = get_gradient_fd(ans=ans, op=op, point=point, grad_pars=[k])
         logging.info(f'Finished computing gradient on {n} qubits')
+        print(f'Finished computing gradient on {n} qubits')
         return {
             'n': n,
             'l': l,
+            'k': k,
             'grad': grad.real,
             'ans': 'mcp'
         }
@@ -127,35 +131,41 @@ def gen_spc_circs(
     num_samples: int,
     time_reversal_symmetry: bool = True
 ):
+    if not time_reversal_symmetry:
+        raise ValueError('How about no')
+
     inputs = []
 
     logging.info('Defining experiments for SPC circs')
 
     for n in qubits_range:
-        for m in range(n):
+        for m in range(1, n):
+            num_pars = comb(n, m) - 1
             for _ in range(num_samples):
-                theta_pars = np.random.uniform(-np.pi, +np.pi, size=comb(n, m) - 1)
+                if time_reversal_symmetry:
+                    k = np.random.randint(0, num_pars)
+                else:
+                    k = np.random.randint(0, 2*num_pars)
+                theta_pars = np.random.uniform(-np.pi, +np.pi, size=num_pars)
+
                 if time_reversal_symmetry:
                     phi_pars = np.zeros(len(theta_pars))
                 else:
-                    phi_pars = np.random.uniform(-np.pi, +np.pi, size=comb(n, m) - 1)
+                    phi_pars = np.random.uniform(-np.pi, +np.pi, size=num_pars)
                 point = np.concatenate((theta_pars, phi_pars))
-                inputs.append({'n': n, 'm': m, 'point': point})
+                inputs.append({'n': n, 'm': m, 'point': point, 'k': k})
     logging.info(f'Defined {len(inputs)} experiments')
 
     # Shuffle so jobs are more homogeneous
     logging.info('Shuffling experiments...')
     shuffle(inputs)
 
-    def grad_comp(n, m, point):
+    def grad_comp(n, m, point, k):
         ans = spc_ansatz(num_qubits=n, num_particles=m)
         op = get_op(n)
         logging.info(f'Computing gradient on {n} qubits')
-        if time_reversal_symmetry:
-            grad_pars = range(comb(n, m) - 1)
-        else:
-            grad_pars = range(2*(comb(n, m) - 1))
-        grad = get_gradient_fd(ans=ans, op=op, point=point, grad_pars=grad_pars)
+        print(point)
+        grad = get_gradient_fd(ans=ans, op=op, point=point, grad_pars=[k])
         logging.info(f'Finished computing gradient on {n} qubits')
         return {
             'n': n,
@@ -166,7 +176,7 @@ def gen_spc_circs(
         }
 
     logging.info('Defining bags')
-    input_bag = db.from_sequence(inputs)
+    input_bag = db.from_sequence(inputs, npartitions=800)
     bag = input_bag.map(lambda kwargs: grad_comp(**kwargs))
     logging.info('Defining dataframe')
     df = bag.to_dataframe()
@@ -183,8 +193,8 @@ if __name__ == '__main__':
     client = Client(cluster)
     client.upload_file('simulator.py')
 
-    num_samples = 50
-    qubits = [4, 6]
+    num_samples = 500
+    qubits = [4, 6, 8]
 
     #df = gen_random_circs(
     #    qubits_range=qubits,
@@ -201,7 +211,7 @@ if __name__ == '__main__':
     #df = gen_mcp_circs(
     #    qubits_range=qubits,
     #    num_samples=num_samples,
-    #    depth_range=[10, 20]
+    #    depth_range=list(range(10, 100, 10)) + list(range(100, 1000, 100)) + [1200, 1400, 1600, 1800, 2000]
     #)
 
     df = df.explode('grad')
