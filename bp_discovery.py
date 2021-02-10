866e88cc5eb8ab1456cb2b425b08f4e2335ca378
George Barron <george.samuel.barron@gmail.com>
2021-02-10 12:17:37 -0500

    Restructured experiments

--- bp_discovery.py
+++ bp_discovery.py
@@ -1,7 +1,8 @@
 import numpy as np
+from functools import lru_cache
 from math import comb
 from dask.distributed import Client, progress, wait
-from typing import List
+from typing import List, Dict
 from math import comb
 from dask_jobqueue import SLURMCluster
 from random import shuffle
@@ -42,60 +43,34 @@ def get_op(n: int) -> Array:
     return res
 
 
-def gen_random_circs(
+def gen_rand_exps(
     qubits_range: List[int],
     num_samples: int,
     layers: List[int]
 ):
     inputs = []
-
     logging.info('Defining experiments for random circs')
-
     for n in qubits_range:
         for l in layers:
             axes = [
                 np.random.choice([0, 1, 2, 3], size=n)
                 for _ in range(l)
             ]
-            for i in range(num_samples):
+            for _ in range(num_samples):
                 point = np.random.uniform(-np.pi, +np.pi, size=l)
                 k = np.random.randint(0, l)
-                inputs.append({'n': n, 'axes': axes, 'point': point, 'k': k})
+                inputs.append({'n': n, 'axes': axes, 'point': point, 'k': k, 'ans': 'rand'})
     logging.info(f'Defined {len(inputs)} experiments')
+    return inputs
 
-    # Shuffle so jobs are more homogeneous
-    logging.info('Shuffling experiments...')
-    shuffle(inputs)
 
-    def grad_comp(n, axes, point):
-        ans = pauli_ansatz(axes=axes)
-        op = get_op(n)
-        logging.info(f'Computing gradient on {n} qubits')
-        grad = get_gradient_fd(ans=ans, op=op, point=point)
-        logging.info(f'Finished computing gradient on {n} qubits')
-        return {
-            'n': n,
-            'l': len(axes),
-            'grad': grad.real,
-            'ans': 'rand'
-        }
-
-    logging.info('Defining bags')
-    input_bag = db.from_sequence(inputs)
-    bag = input_bag.map(lambda kwargs: grad_comp(**kwargs))
-    logging.info('Defining dataframe')
-    df = bag.to_dataframe()
-    return df
-
-def gen_mcp_circs(
+def gen_mcp_exps(
     qubits_range: List[int],
     num_samples: int,
     depth_range: List[int]
 ):
     inputs = []
-
     logging.info('Defining experiments for MCP circs')
-
     for n in qubits_range:
         pool = mcp_g_list(n)
         for l in depth_range:
@@ -104,46 +79,20 @@ def gen_mcp_circs(
                 axes_inds = list(np.random.choice(range(len(pool)), size=l))
                 axes = [pool[i] for i in axes_inds]
                 k = np.random.randint(0, l)
-                inputs.append({'n': n, 'l': l, 'point': point, 'axes': axes, 'k': k})
+                inputs.append({'n': n, 'l': l, 'point': point, 'axes': axes, 'k': k, 'ans': 'mcp'})
     logging.info(f'Defined {len(inputs)} experiments')
-    shuffle(inputs)
-
-    def grad_comp(n, axes, point, l, k):
-        ans = pauli_ansatz(axes)
-        op = get_op(n)
-
-        logging.info(f'Computing gradient on {n} qubits')
-        grad = get_gradient_fd(ans=ans, op=op, point=point, grad_pars=[k])
-        logging.info(f'Finished computing gradient on {n} qubits')
-        print(f'Finished computing gradient on {n} qubits')
-        return {
-            'n': n,
-            'l': l,
-            'k': k,
-            'grad': grad.real,
-            'ans': 'mcp'
-        }
-
-    logging.info('Defining bags')
-    input_bag = db.from_sequence(inputs)
-    bag = input_bag.map(lambda kwargs: grad_comp(**kwargs))
-    logging.info('Defining dataframe')
-    df = bag.to_dataframe()
-    return df
+    return inputs
 
 
-def gen_spc_circs(
+def gen_spc_exps(
     qubits_range: List[int],
     num_samples: int,
     time_reversal_symmetry: bool = True
 ):
     if not time_reversal_symmetry:
         raise ValueError('How about no')
-
     inputs = []
-
     logging.info('Defining experiments for SPC circs')
-
     for n in qubits_range:
         for m in range(1, n):
             num_pars = comb(n, m) - 1
@@ -159,71 +108,75 @@ def gen_spc_circs(
                 else:
                     phi_pars = np.random.uniform(-np.pi, +np.pi, size=num_pars)
                 point = np.concatenate((theta_pars, theta_pars))
-                inputs.append({'n': n, 'm': m, 'point': point, 'k': k})
+                inputs.append({'n': n, 'm': m, 'point': point, 'k': k, 'ans': 'spc', 'time_rev_sym': time_reversal_symmetry})
     logging.info(f'Defined {len(inputs)} experiments')
+    return inputs
 
-    # Shuffle so jobs are more homogeneous
-    logging.info('Shuffling experiments...')
-    shuffle(inputs)
 
-    def grad_comp(n, m, point, k):
-        ans = spc_ansatz(num_qubits=n, num_particles=m)
-        op = get_op(n)
-        logging.info(f'Computing gradient on {n} qubits')
-        grad = get_gradient_fd(ans=ans, op=op, point=point, grad_pars=[k])
-        logging.info(f'Finished computing gradient on {n} qubits')
-        return {
-            'n': n,
-            'm': m,
-            'time_rev_sym': time_reversal_symmetry,
-            'grad': grad.real,
-            'ans': 'spc'
-        }
+def grad_comp(d: Dict) -> float:
+    ans_name = d['ans']
+    op = get_op(d['n'])
+    n = d['n']
 
-    logging.info('Defining bags')
-    input_bag = db.from_sequence(inputs, npartitions=800)
-    bag = input_bag.map(lambda kwargs: grad_comp(**kwargs))
-    logging.info('Defining dataframe')
+    if ans_name == 'rand':
+        ans = pauli_ansatz(axes=d['axes'])
+    elif ans_name == 'mcp':
+        ans = pauli_ansatz(axes=d['axes'])
+    elif ans_name == 'spc':
+        ans = spc_ansatz(num_qubits=n, num_particles=d['m'])
+    
+    logging.info(f'Computing gradient on {n} qubits')
+    grad = get_gradient_fd(ans=ans, op=op, point=d['point']).real
+    return grad
+
+
+def experiments_to_dataframe(exps: List[Dict]):
+    logging.info('Shuffling list...')
+    shuffle(exps)
+    logging.info('Creating bag...')
+    _bag = db.from_sequence(exps, npartitions=800)
+    logging.info('Mapping bag with grad_comp')
+    bag = _bag.map(grad_comp)
+    logging.info('Creating dask DataFrame from bag')
     df = bag.to_dataframe()
+    logging.info('Exploding gradients')
+    df = df.explode('grad')
+    logging.info('Modifying gradient type')
+    df['grad'] = df['grad'].astype(float)
+    logging.info('Returning dask DataFrame...')
     return df
+
 
 if __name__ == '__main__':
     logging.info('Starting dask cluster/client...')
     t_o = time.time()
     np.random.seed(42)
     cluster = SLURMCluster(extra=["--lifetime-stagger", "2m"])
-
     logging.info('Scaling cluster...')
     cluster.scale(jobs=20)
     client = Client(cluster)
     client.upload_file('simulator.py')
 
-    num_samples = 500
-    qubits = [4, 6, 8, 10]
+    # Define experiments
+    num_samples = 50
+    qubits = [4, 6, 8]
 
-    #df = gen_random_circs(
-    #    qubits_range=qubits,
-    #    num_samples=num_samples,
-    #    layers=list(range(10, 100, 10))
-    #)
+    experiments = []
+    experiments.extend(gen_spc_exps(qubits, num_samples))
+    #experiments.extend(gen_mcp_exps(qubits, num_samples, depth_range=[100, 500, 1000, 1500, 2000]))
 
-    df = gen_spc_circs(
-        qubits_range=qubits,
-        num_samples=num_samples,
-        time_reversal_symmetry=True
-    )
+    logging.info('Creating DataFrame with experiments')
+    df = experiments_to_dataframe(experiments)
+    logging.info('DataFrame defined...')
 
-    #df = gen_mcp_circs(
-    #    qubits_range=qubits,
-    #    num_samples=num_samples,
-    #    depth_range=list(range(10, 100, 10)) + list(range(100, 1000, 100)) + [1200, 1400, 1600, 1800, 2000]
-    #)
-
-    logging.info('Exploding gradients')
-    df = df.explode('grad')
-    logging.info('Modifying gradient type')
-    df['grad'] = df['grad'].astype(float)
+    # Do statistics
+    """ Some points in the SPCs have exactly zero gradient. I think this is either due to the
+    first few initial gates potentially acting trivially, but it may be due to some more fundamental
+    symmetry. For now I am just discarding the points that have *exactly* zero gradient.
+    """
+    logging.info('Excluding zero-gradient points')
     df = df[df['grad'] != 0.0]
+
     logging.info('Defining pivot...')
     pivot = df.groupby(['n', 'm']).agg({'grad': ['mean', 'std']})
     logging.info('Dumping to file...')
