d43355ed61d78fcfe8add310008864bfaf69cc54
George Barron <george.samuel.barron@gmail.com>
2021-03-18 16:15:47 -0400

    Adding new tests

--- julia/testing.jl
+++ julia/testing.jl
@@ -6,6 +6,43 @@ using BenchmarkTools
 include("fast_pauli_vec_mult.jl")
 include("simulator.jl")
 
+
+@testset "Test empty ansatz: New" begin
+    d = 5
+    for n=2:5
+        for _=1:10
+            axes = [[rand(0:3) for i=1:n] for j=1:d]
+            theta = [0.0 for j=1:d]
+
+            actual = zeros(ComplexF64, 2^n)
+            actual[1] = 1.0 + 0.0im
+            tmp = zeros(ComplexF64, 2^n)
+            pauli_ansatz_new!(axes, theta, actual, tmp)
+
+            vac = zeros(ComplexF64, 2^n)
+            vac[1] = 1.0 + 0.0im
+
+            @test norm(actual-vac)≈0.0
+        end
+    end
+end
+
+
+@testset "Test empty anastz: Old" begin
+    d = 5
+    for n=2:5
+        for _=1:10
+            axes = [[rand(0:3) for i=1:n] for j=1:d]
+            theta = [0.0 for j=1:d]
+            expected = pauli_ansatz(axes, theta)
+
+            vac = zeros(ComplexF64, 2^n)
+            vac[1] = 1.0 + 0.0im
+            @test norm(vac-expected)≈0.0
+        end
+    end
+end
+
 @testset "Test simulator" begin
     d = 5
     for n=2:5
