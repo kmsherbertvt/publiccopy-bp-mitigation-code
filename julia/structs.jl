8bd2e51c20653cd2ef8ad9b4298fb46b2067cc76
George Barron <george.samuel.barron@gmail.com>
2021-03-29 10:29:21 -0400

    Adding Pauli struct

--- julia/structs.jl
+++ julia/structs.jl
@@ -1,14 +1,35 @@
-struct ADAPTHistory
-    energy::Array{Float64,1}
-    opt_pars::Array{Array{Float64,1},1}
-    gradients::Array{Array{Float64,1},1}
-    max_grad::Array{Float64,1}
-    op_indices::Array{Array{Int64,1},1}
-    halt_reason::string
-end
+struct Pauli{T<:Integer}
+    #=
+    Pauli mask representation of a Pauli string.
+    This type is parametric on `T` so that smaller
+    sized integers can be used to represent the Pauli
+    string, if possible. This also performs a check that
+    the given integers constitute a valid Pauli string.
+    
+    Example:
+    Pauli string: "IIXYZYZIYIZ"
+              id:  11000001010 -> 1546
+               x:  00100000000 -> 256
+               y:  00010100100 -> 164
+               z:  00001010001 -> 81
 
+    =#
+    id::T
+    x::T
+    y::T
+    z::T
 
-struct Operator
-    coeffs::Array{Float64,1}
-    paulis::Array{Array{Int64,1},1}
+    function Pauli{T}(id::T, x::T, y::T, z::T) where T<:Integer
+        s_id = bitstring(id)
+        s_x = bitstring(x)
+        s_y = bitstring(y)
+        s_z = bitstring(z)
+        f = minimum(map(s -> findfirst('1', s), [s_id, s_x, s_y, s_z]))
+        for tup in zip(s_id[f:end], s_x[f:end], s_y[f:end], s_z[f:end])
+            if sum(map(c -> parse(Int, c), tup)) > 1
+                error("Invalid Pauli: $id, $x, $y, $z")
+            end
+        end
+        new(id, x, y, z)
+    end
 end
