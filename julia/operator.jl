e2ab8ba708bbbf1e8c9a5b1dd2a0b977e61a8f7c
George Barron <george.samuel.barron@gmail.com>
2021-03-31 13:17:01 -0400

    Reorganizing functions

---
+++ julia/operator.jl
@@ -0,0 +1,89 @@
+include("pauli.jl")
+
+
+mutable struct Operator
+    paulis::Array{Pauli,1}
+    coeffs::Array{ComplexF64,1}
+end
+
+function group_inds_by_eq(A::Array)
+    l = length(A)
+    grps = Dict{typeof(A[1]), Array{Int64,1}}()
+    hit_elts = []
+    for i=1:l
+        el = A[i]
+        if el in hit_elts
+            append!(grps[el], i)
+        else
+            grps[el] = [i]
+            append!(hit_elts, el)
+        end
+    end
+    return grps
+end
+
+function op_simplify(A::Operator, tol::Float64 = 0.0)
+    l = length(A.paulis)
+
+    # Get rid of zero coeff ops
+    for i=reverse(1:l)
+        if abs(A.coeffs[i]) <= tol
+            deleteat!(A.coeffs, i)
+            deleteat!(A.paulis, i)
+        end
+    end
+
+    # Bring all phases out of Paulis
+    for i=1:l
+        ph = A.paulis[i].phase
+        A.paulis[i].phase = 0
+        alpha = A.coeffs[i]
+        A.coeffs[i] = phase_shift(alpha, ph)
+    end
+
+    # Sum duplicate paulis
+    dict = group_inds_by_eq(A.paulis)
+    new_coeffs = Array{ComplexF64,1}()
+    new_paulis = Array{Pauli,1}()
+
+    for (pauli, inds) in d
+        c = 0.0 + 0.0im
+        for i in inds
+            c += A.coeffs[i]
+        end
+        append!(new_paulis, pauli)
+        append!(new_coeffs, c)
+    end
+    
+    A.coeffs = new_coeffs
+    A.paulis = new_paulis
+end
+
+
+function op_product(A::Operator, B::Operator)
+    new_paulis = Array{Pauli,1}()
+    new_coeffs = Array{ComplexF64,1}()
+
+    for i=1:length(A.paulis)
+        for j=1:length(B.paulis)
+            q = pauli_product(A.paulis[i], B.paulis[j])
+            append!(new_paulis, q)
+            append!(new_coeffs, A.coeffs[i]*B.coeffs[j])
+        end
+    end
+    return Operator(new_paulis, new_coeffs)
+end
+
+
+function exp_val(A::Operator, state::Array{ComplexF64,1}, tmp::Array{ComplexF64})
+    result = 0.0 + 0.0*im
+    pm = [0, 0, 0, 0]
+    for (c,p)=zip(A.coeffs, A.paulis)
+        # This convention is fine since P=dagger(P)
+        # Equiv to (<psi|P)|psi> = <psi|(P|psi>)
+        pm[1] = p.id; pm[2] = p.x; pm[3] = p.y; pm[4] = p.z;
+        pauli_mult!(pm, state, tmp) # tmp <- P.state
+        result += phase_shift(c, p.phase)*dot(conj.(tmp), state)
+    end
+    return result
+end
--- julia/structs.jl
+++ julia/structs.jl
@@ -1,6 +1,5 @@
 using LinearAlgebra
-
-include("fast_pauli_vec_mult.jl")
+import IterTools: enumerate
 
 mutable struct Pauli{T<:Unsigned}
     #=
@@ -52,6 +51,34 @@ mutable struct Pauli{T<:Unsigned}
     end
 end
 
+
+function pauli_string_to_pauli(ps::String, type_out = UInt64)
+    l = zeros(Int64, length(ps))
+    for (i, c)=enumerate(ps)
+        if c == 'I'
+            l[i] = 0
+        elseif c == 'X'
+            l[i] = 1
+        elseif c == 'Y'
+            l[i] = 2
+        elseif c == 'Z'
+            l[i] = 3
+        else
+            error("Invalid character: $c")
+        end
+    end
+    pm = [0, 0, 0, 0]
+    pauli_masks(pm, l)
+    _type_out = unsigned(type_out)
+    return Pauli(
+        _type_out(pm[2]),
+        _type_out(pm[3]),
+        _type_out(pm[4]),
+        _type_out(0),
+    )
+end
+
+
 function pauli_masks(res::Array{Int64,1}, pauli_str::Pauli)
     res[1] = pauli_str.id
     res[2] = pauli_str.x
@@ -59,12 +86,69 @@ function pauli_masks(res::Array{Int64,1}, pauli_str::Pauli)
     res[4] = pauli_str.z
 end
 
+
+function pauli_masks(res::Array{Int64,1}, pauli_str::Array{Int64,1})
+    for (i,ax)=enumerate(reverse(pauli_str))
+        res[ax+1] += 2^(i-1)
+    end
+end
+
+
 function Base.show(io::IO, P::Pauli) 
     num_qubits = maximum(map(i -> ndigits(i, base=2), [P.x, P.y, P.z]))
     xs = bitstring(P.x)[end-num_qubits+1:end]
     ys = bitstring(P.y)[end-num_qubits+1:end]
     zs = bitstring(P.z)[end-num_qubits+1:end]
     print("Pauli(x=$xs, y=$ys, z=$zs)")
+end
+
+
+function phase_shift(alpha::ComplexF64, i::UInt8)
+    if i == 0
+        return     alpha.re + im*alpha.im
+    elseif i == 1
+        return  im*alpha.re - alpha.im 
+    elseif i == 2
+        return    -alpha.re - im*alpha.im
+    else
+        return -im*alpha.re + alpha.im 
+    end
+end
+
+
+function pauli_phase(pm::Array{Int64, 1}, a::Int64)
+    # Compute the phase gamma where P|a> = gamma |b> for
+    # a pauli string P and basis state |a>.
+    # Convention:
+    #   0 -> 1
+    #   1 -> +i
+    #   2 -> -1
+    #   3 -> -i
+    # pm = pauli_mask input
+    x = count_ones((pm[3] | pm[4]) & a) % 2
+    y = count_ones(pm[3]) % 4
+
+    alpha = y
+    beta = 2*x
+
+    return UInt8((alpha+beta) % 4)
+end
+
+
+function pauli_apply(pm::Array{Int64, 1}, a::Int64)
+    # pm = pauli_mask input
+    return xor((pm[2]|pm[3]),a)
+end
+
+
+function pauli_mult!(pm::Array{Int}, state::Array{ComplexF64,1}, result::Array{ComplexF64,1})
+    N = length(state)
+    for i=0:N-1
+        j = pauli_apply(pm, i)
+        phase = (pauli_phase(pm, i)+1) % 4
+        r = state[i+1]
+        result[j+1] = phase_shift(r, phase)
+    end
 end
 
 
@@ -96,130 +180,4 @@ function pauli_product(P::Pauli, Q::Pauli)
     phase += 1*(count_ones(P.x & Q.y) - count_ones(Q.x & P.y))
 
     return Pauli(out_x, out_y, out_z, phase%4)
-end
-
-
-function pauli_string_to_pauli(ps::String, type_out = UInt64)
-    l = zeros(Int64, length(ps))
-    for (i, c)=enumerate(ps)
-        if c == 'I'
-            l[i] = 0
-        elseif c == 'X'
-            l[i] = 1
-        elseif c == 'Y'
-            l[i] = 2
-        elseif c == 'Z'
-            l[i] = 3
-        else
-            error("Invalid character: $c")
-        end
-    end
-    pm = [0, 0, 0, 0]
-    pauli_masks(pm, l)
-    _type_out = unsigned(type_out)
-    return Pauli(
-        _type_out(pm[2]),
-        _type_out(pm[3]),
-        _type_out(pm[4]),
-        _type_out(0),
-    )
-end
-
-
-mutable struct Operator
-    paulis::Array{Pauli,1}
-    coeffs::Array{ComplexF64,1}
-end
-
-function group_inds_by_eq(A::Array)
-    l = length(A)
-    grps = Dict{typeof(A[1]), Array{Int64,1}}()
-    hit_elts = []
-    for i=1:l
-        el = A[i]
-        if el in hit_elts
-            append!(grps[el], i)
-        else
-            grps[el] = [i]
-            append!(hit_elts, el)
-        end
-    end
-    return grps
-end
-
-function op_simplify(A::Operator, tol::Float64 = 0.0)
-    l = length(A.paulis)
-
-    # Get rid of zero coeff ops
-    for i=reverse(1:l)
-        if abs(A.coeffs[i]) <= tol
-            deleteat!(A.coeffs, i)
-            deleteat!(A.paulis, i)
-        end
-    end
-
-    # Bring all phases out of Paulis
-    for i=1:l
-        ph = A.paulis[i].phase
-        A.paulis[i].phase = 0
-        alpha = A.coeffs[i]
-        A.coeffs[i] = phase_shift(alpha, ph)
-    end
-
-    # Sum duplicate paulis
-    dict = group_inds_by_eq(A.paulis)
-    new_coeffs = Array{ComplexF64,1}()
-    new_paulis = Array{Pauli,1}()
-
-    for (pauli, inds) in d
-        c = 0.0 + 0.0im
-        for i in inds
-            c += A.coeffs[i]
-        end
-        append!(new_paulis, pauli)
-        append!(new_coeffs, c)
-    end
-    
-    A.coeffs = new_coeffs
-    A.paulis = new_paulis
-end
-
-
-function op_product(A::Operator, B::Operator)
-    new_paulis = Array{Pauli,1}()
-    new_coeffs = Array{ComplexF64,1}()
-
-    for i=1:length(A.paulis)
-        for j=1:length(B.paulis)
-            q = pauli_product(A.paulis[i], B.paulis[j])
-            append!(new_paulis, q)
-            append!(new_coeffs, A.coeffs[i]*B.coeffs[j])
-        end
-    end
-    return Operator(new_paulis, new_coeffs)
-end
-
-
-function pauli_mult!(pm::Array{Int}, state::Array{ComplexF64,1}, result::Array{ComplexF64,1})
-    N = length(state)
-    for i=0:N-1
-        j = pauli_apply(pm, i)
-        phase = (pauli_phase(pm, i)+1) % 4
-        r = state[i+1]
-        result[j+1] = phase_shift(r, phase)
-    end
-end
-
-
-function exp_val(A::Operator, state::Array{ComplexF64,1}, tmp::Array{ComplexF64})
-    result = 0.0 + 0.0*im
-    pm = [0, 0, 0, 0]
-    for (c,p)=zip(A.coeffs, A.paulis)
-        # This convention is fine since P=dagger(P)
-        # Equiv to (<psi|P)|psi> = <psi|(P|psi>)
-        pm[1] = p.id; pm[2] = p.x; pm[3] = p.y; pm[4] = p.z;
-        pauli_mult!(pm, state, tmp) # tmp <- P.state
-        result += phase_shift(c, p.phase)*dot(conj.(tmp), state)
-    end
-    return result
 end
