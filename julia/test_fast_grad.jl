13fb8c6832da19d60db2fdb3b21800eae02bb051
George Barron <george.samuel.barron@gmail.com>
2021-05-06 14:56:39 -0400

    Changing to XYZ ham

--- julia/test_fast_grad.jl
+++ julia/test_fast_grad.jl
@@ -6,6 +6,7 @@ include("operator.jl")
 include("pauli.jl")
 include("simulator.jl")
 include("pools.jl")
+include("spin_chains.jl")
 
 include("fast_grad.jl")
 
@@ -18,33 +19,27 @@ include("fast_grad.jl")
     tol = 1e-8
 
     _pool = vcat(minimal_complete_pool(n), two_local_pool(n))
-    _h = rand(ComplexF64, 2^n, 2^n)
-    _h += conj(_h)
-    _h /= 2.0
-
-    ham = matrix_to_operator(_h)
+    ham = xyz_model(n, rand(Float64), rand(Float64), rand(Float64), true)
     pars = rand(Float64, N)
-    initial_state = zeros(ComplexF64, 2^n)
-    initial_state[1] = 1.0 + 0.0im
+    initial_state = rand(ComplexF64, 2^n)
+    initial_state /= norm(initial_state)
     ansatz = [sample(_pool) for i=1:N]
 
     # Construction using `fast_grad!`
     result_fg = zeros(Float64, N)
     psi   = zeros(ComplexF64, 2^n)
-    initial_state = zeros(ComplexF64, 2^n)
-    initial_state[1] = 1.0 + 0.0im
     tmp1  = zeros(ComplexF64, 2^n)
     tmp2  = zeros(ComplexF64, 2^n)
-    fast_grad!(ham, ansatz, pars, result_fg, psi, initial_state, tmp1, tmp2)
+    fast_grad!(ham, ansatz, pars, result_fg, psi, copy(initial_state), tmp1, tmp2)
 
     # Construction using finite difference
-    initial_state = zeros(ComplexF64, 2^n)
-    initial_state[1] = 1.0 + 0.0im
     result_fd = zeros(Float64, N)
     tmp1  = zeros(ComplexF64, 2^n)
     tmp2  = zeros(ComplexF64, 2^n)
-    finite_difference!(ham, ansatz, pars, result_fd, initial_state, tmp1, tmp2, eps)
+    finite_difference!(ham, ansatz, pars, result_fd, copy(initial_state), tmp1, tmp2, eps)
 
     # Test difference!
+    println(result_fd)
+    println(result_fg)
     @test norm(result_fd - result_fg) <= tol
 end
