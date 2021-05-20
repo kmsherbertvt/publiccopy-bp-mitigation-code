9b4ac37a951c88554daaee95cdea8f38c4981b6b
George Barron <george.samuel.barron@gmail.com>
2021-05-20 15:44:45 -0400

    Adding more robust test

--- julia/test_fast_grad.jl
+++ julia/test_fast_grad.jl
@@ -45,41 +45,38 @@ Random.seed!(42)
     finite_difference!(ham, ansatz, pars, result_fd, copy(initial_state), tmp1, tmp2, eps)
 
     # Test difference!
-    @show result_fd
-    @show result_fg
     @test norm(result_fd - result_fg) <= tol
 end
 
 @testset "Random Gradient" begin
     # Idea: Random ansatz, random Hamiltonian, random parameters
     # Compute gradient using FD and with `fast_grad!`
-    n = 4
-    N = 10
-    eps = 1e-8
-    tol = 1e-6
+    for N=range(1, 1000, step=50)
+        n = 4
+        eps = 1e-8
+        tol = 1e-5
 
-    _pool = vcat(minimal_complete_pool(n), two_local_pool(n))
-    ham = xyz_model(n, rand(Float64), rand(Float64), rand(Float64), true)
-    pars = rand(Float64, N)
-    initial_state = rand(ComplexF64, 2^n)
-    initial_state /= norm(initial_state)
-    ansatz = [sample(_pool) for i=1:N]
+        _pool = vcat(minimal_complete_pool(n), two_local_pool(n))
+        ham = xyz_model(n, rand(Float64), rand(Float64), rand(Float64), true)
+        pars = rand(Float64, N)
+        initial_state = rand(ComplexF64, 2^n)
+        initial_state /= norm(initial_state)
+        ansatz = [sample(_pool) for i=1:N]
 
-    # Construction using `fast_grad!`
-    result_fg = zeros(Float64, N)
-    psi   = zeros(ComplexF64, 2^n)
-    tmp1  = zeros(ComplexF64, 2^n)
-    tmp2  = zeros(ComplexF64, 2^n)
-    fast_grad!(ham, ansatz, pars, result_fg, psi, copy(initial_state), tmp1, tmp2)
+        # Construction using `fast_grad!`
+        result_fg = zeros(Float64, N)
+        psi   = zeros(ComplexF64, 2^n)
+        tmp1  = zeros(ComplexF64, 2^n)
+        tmp2  = zeros(ComplexF64, 2^n)
+        @time fast_grad!(ham, ansatz, pars, result_fg, psi, copy(initial_state), tmp1, tmp2)
 
-    # Construction using finite difference
-    result_fd = zeros(Float64, N)
-    tmp1  = zeros(ComplexF64, 2^n)
-    tmp2  = zeros(ComplexF64, 2^n)
-    finite_difference!(ham, ansatz, pars, result_fd, copy(initial_state), tmp1, tmp2, eps)
+        # Construction using finite difference
+        result_fd = zeros(Float64, N)
+        tmp1  = zeros(ComplexF64, 2^n)
+        tmp2  = zeros(ComplexF64, 2^n)
+        finite_difference!(ham, ansatz, pars, result_fd, copy(initial_state), tmp1, tmp2, eps)
 
-    # Test difference!
-    println(result_fd)
-    println(result_fg)
-    @test norm(result_fd - result_fg) <= tol
+        # Test difference!
+        @test norm(result_fd - result_fg) <= tol
+    end
 end
