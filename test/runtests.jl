9d35b56a86a3b6c41378abd4bd0a446606df9c3f
sam.barron <sam@sambarron.me>
2022-02-09 11:58:26 -0500

    Update test script

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,16 +1,9 @@
 using AdaptBarren
+using Glob
 using Test
 
-test_files = ["test_adapt.jl",
-    "test_fast_grad.jl",
-    "test_mat_op_conversion.jl",
-    "test_op_mult.jl",
-    "test_operators.jl",
-    "test_pauli_evolve.jl",
-    "test_vqe.jl"]
-
 @testset "AdaptBarren.jl" begin
-    for f in test_files
+    for f in glob("test_*.jl")
         include(f)
     end
 end
