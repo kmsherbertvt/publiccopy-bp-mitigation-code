ca304906ea388e8bb579bf0eba3a0dac29168d4a
sam.barron <sam@sambarron.me>
2022-02-25 12:04:41 -0500

    Added handmade test

--- test/test_op_mult.jl
+++ test/test_op_mult.jl
@@ -27,20 +27,42 @@ using AdaptBarren
     @test norm(actual_result - expected_result) <= 1e-4
 end
 
+@testset "Handmade Example 2" begin
+    H = Operator(
+        [pauli_string_to_pauli("X"), pauli_string_to_pauli("Y"), pauli_string_to_pauli("Z")],
+        [1.0+0.0im, 2.0+0.0im, 3.0+0.0im]
+    )
+    H_mat = [3.0+0.0im +1.0-2.0im 
+             1.0+2.0im -3.0+0.0im]
+    @test norm(operator_to_matrix(H) - H_mat) <= 1e-10
+
+    psi = [1.0+0.0im
+           0.0+2.0im]
+    psi_final = [7.0+2.0im
+                 1.0-4.0im]
+    @test norm(H_mat * psi - psi_final) <= 1e-10
+
+    psi_test = copy(psi)
+    tmp1 = similar(psi_test)
+    tmp2 = similar(psi_test)
+    ham_state_mult!(H, psi_test, tmp1, tmp2)
+    @test norm(psi_test - psi_final) <= 1e-10
+end
+
 @testset "Random Ham Op Mult" begin
-    n = 2
-    mat = rand(ComplexF64, 2^n, 2^n)
-    ham = matrix_to_operator(mat)
+    for _ = 1:5
+        n = 2
+        mat = rand(ComplexF64, 2^n, 2^n)
+        mat = mat + conj(transpose(mat))
+        ham = matrix_to_operator(mat)
 
-    init_state = rand(ComplexF64, 2^n)
-    init_state /= norm(init_state)
+        psi_init = rand(ComplexF64, 2^n)
+        psi_final = mat * psi_init
 
-    expected_result = mat * init_state
+        tmp1 = similar(psi_final)
+        tmp2 = similar(psi_final)
+        ham_state_mult!(ham, psi_init, tmp1, tmp2)
 
-    actual_result = copy(init_state)
-    tmp1 = similar(actual_result)
-    tmp2 = similar(actual_result)
-    ham_state_mult!(ham, actual_result, tmp1, tmp2)
-
-    @test norm(actual_result - expected_result) <= 1e-4
+        @test norm(psi_final - psi_init) <= 1e-10
+    end
 end
