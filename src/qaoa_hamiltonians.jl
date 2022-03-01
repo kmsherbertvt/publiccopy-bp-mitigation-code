543eac1e7ce7599032a68ddc980048915f10136e
sam.barron <sam@sambarron.me>
2022-03-01 11:11:14 -0500

    Adding mixer

--- src/qaoa_hamiltonians.jl
+++ src/qaoa_hamiltonians.jl
@@ -1,6 +1,20 @@
 using Random
 using Erdos
 
+
+function _id_x_str(n::Int, k::Int)
+    s = repeat(["I"], n)
+    s[k] = "X"
+    return join(s)
+end
+
+function qaoa_mixer(n::Int)
+    paulis = [pauli_string_to_pauli(_id_x_str(n, k)) for k in range(1,n)]
+    coeffs = repeat([1.0], n)
+    return Operator(paulis, coeffs)
+end
+
+
 function max_cut_hamiltonian(g)
     #if has_edge_property(g, "weight")
     #    error("Graph must be unweighted")
