228695918806385e5c45386b61f53568374956f7
sam.barron <sam@sambarron.me>
2022-04-06 15:46:57 -0400

    Adding comparison, looks like it's working maybe???

--- examples/adapt_qaoa/adapt_qaoa.jl
+++ examples/adapt_qaoa/adapt_qaoa.jl
@@ -26,6 +26,10 @@ function safe_floor(x, eps = 1e-5, delta = 1e-10)
     return x
 end
 
+#test_ham_vec = [0.0, -1.779640524849019, -0.9260288163486794, -1.9681563115877658, -1.5418617675114605, -2.6440667824502255, -2.1043764787001074, -2.469068464028941, -1.4482061210959358, -2.7084856199552063, -2.3316058016329757, -2.8543722708823145, -2.6037327216173747, -3.186576710566392, -3.123618296994383, -2.9689492563334676, -1.6430595132822896, -2.9277699712739658, -2.435395389482706, -2.98259281786445, -2.502309525394874, -3.1095844734762967, -2.931131296435258, -2.8008932149067483, -2.1243100795902614, -2.8896595115921895, -2.8740168199790386, -2.9018532223710345, -2.597224924712824, -2.685138846804498, -2.983417559941569, -2.3338184524233103, -2.3338184524233103, -2.983417559941569, -2.685138846804498, -2.597224924712824, -2.9018532223710345, -2.8740168199790386, -2.8896595115921895, -2.1243100795902614, -2.8008932149067483, -2.931131296435258, -3.1095844734762967, -2.502309525394874, -2.98259281786445, -2.435395389482706, -2.9277699712739658, -1.6430595132822896, -2.9689492563334676, -3.123618296994383, -3.186576710566392, -2.6037327216173747, -2.8543722708823145, -2.3316058016329757, -2.7084856199552063, -1.4482061210959358, -2.469068464028941, -2.1043764787001074, -2.6440667824502255, -1.5418617675114605, -1.9681563115877658, -0.9260288163486794, -1.779640524849019, 0.0]
+#test_ham = matrix_to_operator(diagm(test_ham_vec))
+#op_chop!(test_ham, 1e-10)
+
 # Hyperparameters
 num_samples = 20
 opt_alg = "LD_LBFGS"
@@ -53,6 +57,7 @@ end
 
 function run_adapt_qaoa(n, hamiltonian)
     ground_state_energy = minimum(real(diag(operator_to_matrix(hamiltonian))))
+    #@show ground_state_energy
 
     pool = two_local_pool(n)
     pool = map(p -> Operator([p], [1.0]), pool)
@@ -60,7 +65,12 @@ function run_adapt_qaoa(n, hamiltonian)
 
     initial_state = ones(ComplexF64, 2^n) / sqrt(2^n)
     initial_state /= norm(initial_state)
-    callbacks = Function[ParameterStopper(max_pars), MaxGradientStopper(max_grad)]
+    callbacks = Function[
+        ParameterStopper(max_pars), 
+        MaxGradientStopper(max_grad),
+        #EnergyPrinter(),
+        #MaxGradientPrinter()
+        ]
 
     result = adapt_qaoa(hamiltonian, pool, n, opt_alg, callbacks; initial_parameter=1e-2, initial_state=initial_state, path=path)
 
@@ -81,6 +91,7 @@ lk = ReentrantLock()
 println("Starting simulations...")
 Threads.@threads for i in ProgressBar(1:num_samples, printing_delay=0.1)
     hamiltonian = random_regular_max_cut_hamiltonian(n, d)
+    #hamiltonian = test_ham
     _res_qaoa = run_qaoa(n, hamiltonian);
     _res_adapt = run_adapt_qaoa(n, hamiltonian);
 
