28e18b6c4196a8cdb5232a526f52715ed36afc4e
sam.barron <sam@sambarron.me>
2022-04-13 15:19:28 -0400

    Updating plots and large-scale script

--- examples/adapt_qaoa/adapt_qaoa.jl
+++ examples/adapt_qaoa/adapt_qaoa.jl
@@ -5,10 +5,21 @@ using AdaptBarren
 using StatsPlots
 using NLopt
 using Random
+using CSV
 using ProgressBars
 
 Random.seed!(42)
 rng = MersenneTwister(14)
+
+function mean(x) return sum(x)/length(x) end
+function safe_floor(x::Float64, eps=1e-15, delta=1e-8)
+    if x <= -delta error("Too negative...") end
+    if x <= 0.0
+        return eps
+    else
+        return x
+    end
+end
 
 # Hyperparameters
 num_samples = 20
@@ -60,54 +71,53 @@ results_qaoa = []
 results_adapt = []
 df = DataFrame(seed=[], alg=[], layer=[], err=[], n=[])
 
-n_max = 14
-d = n-1
+n_min = 4
+n_max = 8
 
 lk = ReentrantLock()
 println("Starting simulations...")
 Threads.@threads for i in ProgressBar(1:num_samples, printing_delay=0.1)
-    hamiltonian = random_regular_max_cut_hamiltonian(n, d)
-    gse = minimum(real(diag(operator_to_matrix(hamiltonian))))
-    _res_qaoa = run_qaoa(n, hamiltonian);
-    hist_adapt, _res_adapt = run_adapt_qaoa(n, hamiltonian);
+    for n=n_min:2:n_max
+        d = n-1
+        hamiltonian = random_regular_max_cut_hamiltonian(n, d)
+        gse = minimum(real(diag(operator_to_matrix(hamiltonian))))
+        _res_qaoa = run_qaoa(n, hamiltonian);
+        hist_adapt, _res_adapt = run_adapt_qaoa(n, hamiltonian);
 
-    lock(lk) do
-        #loop over results in adapt and append to df
-        for (k,en)=enumerate(hist_adapt.energy)
-            push!(df, Dict(:seed=>i, :alg=>"ADAPT", :layer=>k+1, :err=>en-gse, :n=>n))
+        lock(lk) do
+            #loop over results in adapt and append to df
+            for (k,en)=enumerate(hist_adapt.energy)
+                push!(df, Dict(:seed=>i, :alg=>"ADAPT", :layer=>k+1, :err=>safe_floor(en-gse), :n=>n))
+            end
+            #loop over results in qaoa and append to df
+            for (k,en)=enumerate(_res_qaoa)
+                # double the number of parameters since k here measures p
+                push!(df, Dict(:seed=>i, :alg=>"QAOA", :layer=>2*k, :err=>safe_floor(en-gse), :n=>n))
+            end
+
+            # Also collect here
+            push!(results_adapt, _res_adapt)
+            push!(results_qaoa, _res_qaoa)
         end
-        #loop over results in qaoa and append to df
-        for (k,en)=enumerate(_res_qaoa)
-            # double the number of parameters since k here measures p
-            push!(df, Dict(:seed=>i, :alg=>"QAOA", :layer=>2*k, :err=>en-gse, :n=>n))
-        end
-
-        # Also collect here
-        push!(results_adapt, _res_adapt)
-        push!(results_qaoa, _res_qaoa)
-    end
-
-end
-println("Done with simulations, plotting...")
-
-function safe_floor(x::Float64, eps=1e-15, delta=1e-8)
-    if x <= -delta error("Too negative...") end
-    if x <= 0.0
-        return eps
-    else
-        return x
     end
 end
+println("Done with simulations, dumping data...")
+CSV.write("data.csv", df)
+println("Plotting...")
 
 ### Plotting
 using Plots; gr()
-plot()
-@df filter(:alg => ==("QAOA"), df) plot!(:layer, :err, group=:seed, color=:blue, yaxis=:log, xlim=(2,max_pars), left_margin=10Plots.mm, legend=false)
-@df filter(:alg => ==("ADAPT"), df) plot!(:layer, :err, group=:seed, color=:red, yaxis=:log, xlim=(2,max_pars), left_margin=10Plots.mm, legend=false)
-savefig("test_qaoa_comp.pdf")
 
-plot()
-gdf = groupby(df, [:layer, :alg])
-function mean(x) return sum(x)/length(x) end
-@df combine(gdf, :err => (x -> 10^mean(log10.(safe_floor.(x)))) => :err_mean) plot(:layer, :err_mean, group=:alg, yaxis=:log, xlim=(2,max_pars), left_margin=10Plots.mm, legend=false)
-savefig("test_qaoa_comp_mean.pdf")
+for n_=n_min:2:n_max
+    df_n = filter(:n => n -> n==n_, df)
+    n = n_
+    plot()
+    @df filter(:alg => ==("QAOA"), df_n) plot!(:layer, :err, group=:seed, color=:blue, yaxis=:log, xlim=(2,max_pars), left_margin=10Plots.mm, legend=false)
+    @df filter(:alg => ==("ADAPT"), df_n) plot!(:layer, :err, group=:seed, color=:red, yaxis=:log, xlim=(2,max_pars), left_margin=10Plots.mm, legend=false)
+    savefig("test_qaoa_comp_$n.pdf")
+
+    plot()
+    gdf_n = groupby(df_n, [:layer, :alg])
+    @df combine(gdf_n, :err => (x -> 10^mean(log10.(safe_floor.(x)))) => :err_mean) plot(:layer, :err_mean, group=:alg, yaxis=:log, xlim=(2,max_pars), left_margin=10Plots.mm, legend=true)
+    savefig("test_qaoa_comp_mean_$n.pdf")
+end
