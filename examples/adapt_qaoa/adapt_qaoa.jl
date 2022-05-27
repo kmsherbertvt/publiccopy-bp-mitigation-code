b26411c38bd22b0a3262b59314355fd8f052321c
sam.barron <sam@sambarron.me>
2022-05-27 13:23:26 -0400

    Looks like it's working and parallelized

--- examples/adapt_qaoa/adapt_qaoa.jl
+++ examples/adapt_qaoa/adapt_qaoa.jl
@@ -29,7 +29,7 @@ println("staring script..."); flush(stdout)
 @everywhere max_grad = 1e-4
 @everywhere path="test_data"
 @everywhere n_min = 4
-@everywhere n_max = 10
+@everywhere n_max = 14
 
 
 @everywhere function run_adapt_qaoa(seed, pool_name, n)
@@ -54,7 +54,7 @@ println("staring script..."); flush(stdout)
     initial_state /= norm(initial_state)
     callbacks = Function[ ParameterStopper(max_pars)]
 
-    result = adapt_qaoa(hamiltonian, pool, n, opt_dict, callbacks; initial_parameter=1e-2, initial_state=initial_state, path=path)
+    result = adapt_qaoa(hamiltonian, pool_name, n, opt_dict, callbacks; initial_parameter=1e-2, initial_state=initial_state, path=path)
     t_f = time()
     dt = t_f - t_0
     println("Finished n=$n, seed=$seed with alg=$pool_name in $dt seconds"); flush(stdout)
@@ -67,6 +67,7 @@ println("staring script..."); flush(stdout)
         gse_overlap = ground_state_overlap(ham_vec, d[:opt_state])
         push!(df, Dict(:seed=>seed, :alg=>pool, :layer=>k, :err=>en_err, :n=>n, :overlap=>gse_overlap, :time=>dt))
     end
+    return df
 end
 
 # Parallel Debug
@@ -74,15 +75,22 @@ println("Num procs: $(nprocs())")
 println("Num workers: $(nworkers())")
 
 # Main Loop
-fn_inputs = collect(product(1:num_samples, ["2l", "qaoa"], 4:2:n_max))
-println("starting simulations..."); flush(stdout)
-#results = map(i -> run_adapt_qaoa(i...), fn_inputs)
-results = pmap(i -> run_adapt_qaoa(i...), fn_inputs)
+global df = DataFrame(seed=[], alg=[], layer=[], err=[], n=[], overlap=[], time=[])
+for n=4:2:n_max
+    fn_inputs = collect(product(1:num_samples, ["2l", "qaoa"], [n]))
+    println("starting simulations..."); flush(stdout)
+    #results = map(i -> run_adapt_qaoa(i...), fn_inputs)
+    t_0 = time()
+    results = pmap(i -> run_adapt_qaoa(i...), fn_inputs)
+    t_f = time()
+    dt = t_f - t_0
+    println("The walltime for the n=$n samples was $dt seconds")
 
-println("concatenating results..."); flush(stdout)
-df = vcat(results...)
-println("dumping data..."); flush(stdout)
-CSV.write("data.csv", df)
+    println("concatenating results..."); flush(stdout)
+    global df = vcat(df, results...)
+    println("dumping data..."); flush(stdout)
+    CSV.write("data.csv", df)
+end
 
 #println("Plotting..."); flush(stdout)
 ### Plotting
