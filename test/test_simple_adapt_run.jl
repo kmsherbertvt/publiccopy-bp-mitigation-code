680c8d6e570a2aa5eecb5a319c8e07c82e2e0cd1
sam.barron <sam@sambarron.me>
2022-05-27 10:13:59 -0400

    Updating debug script

--- test/test_simple_adapt_run.jl
+++ test/test_simple_adapt_run.jl
@@ -1,37 +1,44 @@
 using LinearAlgebra
 using Random
 using Test
+using DataFrames
 using AdaptBarren
 
 @testset "ADAPT Simple Run" begin
-    n = 4
+    n = 2
     operator = random_regular_max_cut_hamiltonian(n, n-1)
-    op_simplify!(operator)
-    hamiltonian = operator_to_matrix(operator)
-    ground_state_energy = minimum(real(diag(hamiltonian)))
+    
+    gse = get_ground_state(operator)
+    ham_vec = real(diagonal_operator_to_vector(operator))
 
     pool = two_local_pool(n)
     optimizer = "LD_LBFGS"
     callbacks = Function[ParameterStopper(10), MaxGradientStopper(1e-10)]
             
-    state = rand(ComplexF64, 2^n)
+    state = ones(ComplexF64, 2^n)
     state /= norm(state)
 
     result = adapt_vqe(operator, pool, n, optimizer, callbacks; initial_parameter=0.0, initial_state=state)
 
     en_adapt = last(result.energy)
 
-    en_err = abs(ground_state_energy - en_adapt)
+    en_err = abs(gse - en_adapt)
 
-    println("Energy")
-    println(result.energy)
-    println("\n\n\n")
-
-    println("Result")
-    println(result)
-    println("\n\n\n")
-
-    #for (k,d)=enumerate(result)
-    #    println(d)
-    #end
+    df = DataFrame(layer=[], err=[], n=[], overlap=[])
+    num_layers = length(result)
+    for k = 1:num_layers
+        d = result[k]
+        #println(d)
+        en_err = safe_floor(d[:energy]-gse)
+        gse_overlap = ground_state_overlap(ham_vec, d[:opt_state])
+        push!(df, Dict(:layer=>k, :err=>en_err, :n=>n, :overlap=>gse_overlap))
+    end
+    #println("\n\n\n")
+    #println(df)
+    #println("\n\n\n")
+    #println(result.energy)
+    #println("\n\n\n")
+    for (i,st)=enumerate(result.opt_state)
+        #println("Layer $i: $st")
+    end
 end
