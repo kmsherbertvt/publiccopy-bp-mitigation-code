2ad41c224dbd3114ce69e495aed2c51ffc10c285
sam.barron <sam@sambarron.me>
2022-05-31 11:25:17 -0400

    Updating plotting

--- examples/adapt_qaoa/plotting.jl
+++ examples/adapt_qaoa/plotting.jl
@@ -7,36 +7,25 @@ using Statistics
 gr();
 
 FIGS_DIR = "./figs"
+BASE_NAME = "qaoa_max_cut_"
 DATA_FILE = "./data.csv"
 
 df = DataFrame(CSV.File(DATA_FILE));
 gdf = groupby(df, [:n, :layer, :alg]);
-cdf = combine(gdf, [:err => mean => :err_mean, :overlap => mean => :overlap_mean]);
+cdf = combine(gdf, [:err => mean => :err_mean, :overlap => mean => :overlap_mean, :rel_err => mean => :rel_err_mean, :apprx => mean => :apprx_mean]);
 
-# yaxis=:log, 
-plot_overlap_adapt = @df filter(:alg => ==("2l"), cdf) plot(:layer, :overlap_mean, group=:n, title="ADAPT-QAOA", legend=:topleft, ylabel="Overlap (summed)");
-plot_overlap_qaoa = @df filter(:alg => ==("qaoa"), cdf) plot(:layer, :overlap_mean, group=:n, title="QAOA", legend=:topleft);
+plot_adapt = @df filter(:alg => ==("2l"), cdf)   plot(:layer, :overlap_mean, group=:n, legend=:topleft, ylim=(0,1), title="ADAPT-QAOA", ylabel="Overlap (summed)");
+plot_qaoa  = @df filter(:alg => ==("qaoa"), cdf) plot(:layer, :overlap_mean, group=:n, legend=:topleft, ylim=(0,1), title="QAOA");
+plot(plot_adapt, plot_qaoa, layout=(2,1)); savefig("$FIGS_DIR/$(BASE_NAME)_overlap.pdf")
 
-plot_energy_adapt = @df filter(:alg => ==("2l"), cdf) plot(:layer, :err_mean, group=:n, legend=:bottomleft, yaxis=:log, ylims=(1e-10, 10), xlabel="p", ylabel="Energy Error");
-plot_energy_qaoa = @df filter(:alg => ==("qaoa"), cdf) plot(:layer, :err_mean, group=:n, legend=:bottomleft, yaxis=:log, ylims=(1e-10, 10), xlabel="p");
-plot(plot_overlap_adapt, plot_overlap_qaoa, plot_energy_adapt, plot_energy_qaoa, layout=(2,2), left_margin=15Plots.mm, bottom_margin=6Plots.mm)
-savefig("$FIGS_DIR/fig.pdf")
+plot_adapt = @df filter(:alg => ==("2l"), cdf)   plot(:layer, :apprx_mean, group=:n, legend=:topleft, ylim=(0.5,1), title="ADAPT-QAOA", ylabel="Approximation Ratio");
+plot_qaoa  = @df filter(:alg => ==("qaoa"), cdf) plot(:layer, :apprx_mean, group=:n, legend=:topleft, ylim=(0.5,1), title="QAOA");
+plot(plot_adapt, plot_qaoa, layout=(2,1)); savefig("$FIGS_DIR/$(BASE_NAME)_approx_ratio.pdf")
 
-#println("Plotting..."); flush(stdout)
-### Plotting
-#using Plots; gr()
-#
-#for n_=n_min:2:n_max
-#    df_n = filter(:n => n -> n==n_, df)
-#    n = n_
-#    plot()
-#    @df filter(:alg => ==("QAOA"), df_n) plot!(:layer, :err, group=:seed, color=:blue, yaxis=:log, xlim=(2,max_pars), left_margin=10Plots.mm, legend=false)
-#    @df filter(:alg => ==("ADAPT"), df_n) plot!(:layer, :err, group=:seed, color=:red, yaxis=:log, xlim=(2,max_pars), left_margin=10Plots.mm, legend=false)
-#    savefig("test_qaoa_comp_$n.pdf")
-#
-#    plot()
-#    gdf_n = groupby(df_n, [:layer, :alg])
-#    @df combine(gdf_n, :err => (x -> 10^mean(log10.(safe_floor.(x)))) => :err_mean) plot(:layer, :err_mean, group=:alg, yaxis=:log, xlim=(2,max_pars), left_margin=10Plots.mm, legend=true)
-#    savefig("test_qaoa_comp_mean_$n.pdf")
-#end
-#
+plot_adapt = @df filter(:alg => ==("2l"), cdf)   plot(:layer, :err_mean, group=:n, legend=:topleft, ylim=(1e-10, 1), yaxis=:log, title="ADAPT-QAOA", ylabel="Energy Error");
+plot_qaoa  = @df filter(:alg => ==("qaoa"), cdf) plot(:layer, :err_mean, group=:n, legend=:topleft, ylim=(1e-10, 1), yaxis=:log, title="QAOA");
+plot(plot_adapt, plot_qaoa, layout=(2,1)); savefig("$FIGS_DIR/$(BASE_NAME)_energy_error.pdf")
+
+plot_adapt = @df filter(:alg => ==("2l"), cdf)   plot(:layer, :rel_err_mean, group=:n, legend=:topleft, ylim=(1e-10, 1), yaxis=:log, title="ADAPT-QAOA", ylabel="Relative Error");
+plot_qaoa  = @df filter(:alg => ==("qaoa"), cdf) plot(:layer, :rel_err_mean, group=:n, legend=:topleft, ylim=(1e-10, 1), yaxis=:log, title="QAOA");
+plot(plot_adapt, plot_qaoa, layout=(2,1)); savefig("$FIGS_DIR/$(BASE_NAME)_relative_energy_error.pdf")
