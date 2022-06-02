20a7af03a05cd3953ddff1e9c888e71847ceddb8
sam.barron <sam@sambarron.me>
2022-06-02 17:44:29 -0400

    Adding string formatting

--- src/utils.jl
+++ src/utils.jl
@@ -1,5 +1,33 @@
 using StatsBase
 
+_PAULI_DICT = Dict(0 => "I", 1 => "X", 2 => "Y", 3 => "Z")
+
+function String(pauli::Pauli{T}; zero_index = false) where T<:UInt
+    ax = reverse(pauli_to_axes(pauli, num_qubits(pauli)))
+    s = "["
+    for (i,a)=enumerate(ax)
+        if zero_index
+            i -= 1
+        end
+        if a != 0
+            s = s * "$(_PAULI_DICT[a])$i "
+        end
+    end
+    s = strip(s, [' '])
+    s = s * "]"
+    return s
+end
+
+function String(op::Operator)
+    s = "Op["
+    for (p,c)=zip(op.paulis, op.coeffs)
+        term = "($c)$(String(p)) + "
+        s = s * term
+    end
+    s = strip(s, [' ', '+'])
+    s = s * "]"
+end
+
 function safe_floor(x::Float64, eps=1e-15, delta=1e-8)
     if x <= -delta error("Too negative...") end
     if x <= 0.0
