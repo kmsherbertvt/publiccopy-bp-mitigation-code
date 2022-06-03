715c8e797e00a056b9eec541a259a8ed886d4a2b
sam.barron <sam@sambarron.me>
2022-06-03 15:45:05 -0400

    Updating skeleton for script

--- examples/bp_sampling/bp_sampling.jl
+++ examples/bp_sampling/bp_sampling.jl
@@ -28,62 +28,48 @@ println("staring script..."); flush(stdout)
 @everywhere path="test_data"
 @everywhere n_min = 4
 @everywhere n_max = 14
+@everywhere num_grad_samples = 500
 
-
-@everywhere function run_instance(seed, pool_name, adaptive, n)
-
+# Main functions
+""" These should take hyperparameter inputs and return dictionary
+outputs with results for analysis and sampling
+"""
+@everywhere function main_adapt(n, ham, pool)
+    initial_state = uniform_state(n)
+    adapt_vqe()
 end
 
+@everywhere function main_adapt_qaoa(n, ham, pool)
+    initial_state = uniform_state(n)
+    adapt_qaoa()
+end
 
-@everywhere function run_adapt_qaoa(seed, pool_name, n)
-    t_0 = time()
-    d = n-1
+@everywhere function main_vqe(n, ham, ansatz)
+    initial_state = uniform_state(n)
+    VQE()
+end
+
+@everywhere function main_qaoa(n, ham, p)
+    initial_state = uniform_state(n)
+    initial_point = ...
+    path = nothing
+    energy, optimal_parameters, ret, eval_count = QAOA()
+end
+
+# Main sampling
+""" This should take in inputs based on results from (ADAPT-)VQE
+runs and then return statistics on the sampling.
+"""
+@everywhere function run_sampling()
+    VQE(num_samples=num_grad_samples)
+end
+
+# Main function
+@everywhere function run_instance(seed, n)
     rng = MersenneTwister(seed)
-    hamiltonian = random_regular_max_cut_hamiltonian(n, d; rng=rng)
+    hamiltonian = random_regular_max_cut_hamiltonian(n, n-1; rng=rng, weighted=true)
 
-    pool = Vector{Operator}()
-    push!(pool, qaoa_mixer(n))
-    if pool_name == "2l"
-        append!(pool, map(p -> Operator([p], [1.0]), two_local_pool(n)))
-    elseif pool_name == "qaoa"
-        1
-    else
-        error("Invalid pool: $pool")
-    end
 
-    ham_vec = real(diagonal_operator_to_vector(hamiltonian))
-    gse = get_ground_state(hamiltonian)
-    gap = get_energy_gap(hamiltonian)
-
-    initial_state = ones(ComplexF64, 2^n) / sqrt(2^n)
-    initial_state /= norm(initial_state)
-    callbacks = Function[ ParameterStopper(max_pars)]
-
-    result = adapt_qaoa(hamiltonian, pool, n, opt_dict, callbacks; initial_parameter=1e-2, initial_state=initial_state, path=path)
-    t_f = time()
-    dt = t_f - t_0
-    println("Finished n=$n, seed=$seed with alg=$pool_name in $dt seconds"); flush(stdout)
-
-    num_layers = length(result)
-    df = DataFrame(seed=[], alg=[], layer=[], err=[], n=[], overlap=[], time=[], rel_err=[], apprx=[])
-    for k = 1:num_layers
-        d = result[k]
-        en_err = safe_floor(d[:energy]-gse)
-        rel_en_err = en_err / gap
-        gse_overlap = ground_state_overlap(ham_vec, d[:opt_state])
-        push!(df, Dict(
-            :seed=>seed, 
-            :alg=>pool_name, 
-            :apprx=>d[:energy]/gse,
-            :layer=>k, 
-            :err=>en_err,
-            :rel_err=>rel_en_err,
-            :n=>n, 
-            :overlap=>gse_overlap, 
-            :time=>dt
-            ))
-    end
-    return df
 end
 
 # Parallel Debug
@@ -92,18 +78,3 @@ println("Num workers: $(nworkers())")
 
 # Main Loop
 global df = DataFrame(seed=[], alg=[], layer=[], err=[], n=[], overlap=[], time=[], rel_err=[], apprx=[])
-for n=4:2:n_max
-    fn_inputs = collect(product(1:num_samples, ["2l", "qaoa"], [n]))
-    println("starting simulations..."); flush(stdout)
-    #results = map(i -> run_adapt_qaoa(i...), fn_inputs)
-    t_0 = time()
-    results = pmap(i -> run_adapt_qaoa(i...), fn_inputs)
-    t_f = time()
-    dt = t_f - t_0
-    println("The walltime for the n=$n samples was $dt seconds")
-
-    println("concatenating results..."); flush(stdout)
-    global df = vcat(df, results...)
-    println("dumping data..."); flush(stdout)
-    CSV.write("data.csv", df)
-end
