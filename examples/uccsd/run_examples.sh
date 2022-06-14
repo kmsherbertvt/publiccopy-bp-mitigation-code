aa3010a5196df212cadd78e101e782c338886580
sam.barron <sam@sambarron.me>
2022-06-14 10:54:52 -0400

    Automating tests, looks like output is consistent from before

---
+++ examples/uccsd/run_examples.sh
@@ -0,0 +1,4 @@
+julia --project=@. test_uccsd.jl H4 > output_h4.txt
+julia --project=@. test_uccsd.jl H6 > output_h6_1A.txt
+julia --project=@. test_uccsd.jl H8 > output_h8.txt
+julia --project=@. test_uccsd.jl N2 > output_n2.txt
--- examples/uccsd/test_uccsd.jl
+++ examples/uccsd/test_uccsd.jl
@@ -37,44 +37,51 @@ end
 
 opt = "LD_LBFGS"
 
-# H_6 at 1A
-#num_qubits = 12
-#hamiltonian = get_ham("h6_1A.ham", num_qubits)
-#(pool_labels, pool) = uccsd_pool(num_qubits, 6)
-#hf_ind = Int(0b111111000000) + 1
-#en_fci = -3.236066279892345
 
-# N_2
-num_qubits = 20
-hamiltonian = get_ham("n2.ham", num_qubits)
-(pool_labels, pool) = uccsd_pool(num_qubits, 10)
-hf_ind = Int(0b11111111110000000000) + 1
-en_fci = -107.657038063455
-
-
-# H_8
-num_qubits = 16
-hamiltonian = get_ham("h8.ham", num_qubits)
-(pool_labels, pool) = uccsd_pool(num_qubits, 8)
-hf_ind = Int(0b1111111100000000) + 1
-en_fci = -4.307571601998721
-
-# H_4
-num_qubits = 8
-hamiltonian = get_ham("h4.ham", num_qubits)
-(pool_labels, pool) = uccsd_pool(num_qubits, 4)
-hf_ind = Int(0b11110000) + 1
-en_fci = -2.1663874486347625
+if ARGS[1] == "N2"
+    # N_2
+    num_qubits = 20
+    hamiltonian = get_ham("n2.ham", num_qubits)
+    (pool_labels, pool) = uccsd_pool(num_qubits, 10)
+    hf_ind = Int(0b11111111110000000000) + 1
+    en_fci = -107.657038063455
+elseif ARGS[1] == "H6"
+    # H_6 at 1A
+    num_qubits = 12
+    hamiltonian = get_ham("h6_1A.ham", num_qubits)
+    (pool_labels, pool) = uccsd_pool(num_qubits, 6)
+    hf_ind = Int(0b111111000000) + 1
+    en_fci = -3.236066279892345
+elseif ARGS[1] == "H8"
+    # H_8
+    num_qubits = 16
+    hamiltonian = get_ham("h8.ham", num_qubits)
+    (pool_labels, pool) = uccsd_pool(num_qubits, 8)
+    hf_ind = Int(0b1111111100000000) + 1
+    en_fci = -4.307571601998721
+elseif ARGS[1] == "H4"
+    # H_4
+    num_qubits = 8
+    hamiltonian = get_ham("h4.ham", num_qubits)
+    (pool_labels, pool) = uccsd_pool(num_qubits, 4)
+    hf_ind = Int(0b11110000) + 1
+    en_fci = -2.1663874486347625
+else
+    error("Invalid input: $(ARGS[1])")
+end
 
 initial_state = zeros(ComplexF64, 2^num_qubits)
 initial_state[hf_ind] = 1.0 + 0.0im
 println("Number of operators in the pool is: " * string(length(pool)))
 
+max_time = 14400 # Max duration is 4 hours
+
 callbacks = Function[
     MaxGradientStopper(1e-8),
     FloorStopper(en_fci, 1e-10),
-    ParameterStopper(300),
+    ParameterStopper(30),
     EnergyErrorPrinter(en_fci),
+    DeadlineStopper(max_time),
     MaxGradientPrinter(),
     ParameterPrinter(),
     OperatorIndexPrinter(pool_labels)
