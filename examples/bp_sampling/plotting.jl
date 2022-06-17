d644f8b51d14a22b2ba5eae4ded725219968f1cf
sam.barron <sam@sambarron.me>
2022-06-17 14:30:11 -0400

    Initial start to plotting script

--- examples/bp_sampling/plotting.jl
+++ examples/bp_sampling/plotting.jl
@@ -14,16 +14,11 @@ df_en = vcat([DataFrame(CSV.File("$(DATA_DIR)/data_en_$(n).csv.test")) for n=[4,
 df_grad = vcat([DataFrame(CSV.File("$(DATA_DIR)/data_grad_$(n).csv.test")) for n=[4,6]]...)
 
 df_res = vcat([DataFrame(CSV.File("$(DATA_DIR)/data_res_$(n).csv.test")) for n=[4,6]]...)
-#transform!(df_res, :energies => x -> eval(Meta.parse(x)) => :energies)
-
-cols_to_eval = [
-    :energies,
-    :max_grads,
-    :opt_pars,
-    :energy_errors,
-    :approx_ratio,
-    :relative_error,
-    :ground_state_overlaps,
-]
+cols_to_eval = [ :energies, :max_grads, :opt_pars, :energy_errors, :approx_ratio, :relative_error, :ground_state_overlaps, ]
 transform!(df_res, cols_to_eval .=> ByRow(x -> eval(Meta.parse(x))) .=> cols_to_eval)
 
+function plot_1()
+    gdf = groupby(df_en, )
+end
+
+plot_1()
