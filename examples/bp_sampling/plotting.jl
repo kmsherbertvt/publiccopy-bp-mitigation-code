5d52ad0454712fdc560ece35cedfa31eaf1da1fe
sam.barron <sam@sambarron.me>
2022-06-17 14:16:07 -0400

    Import data

--- examples/bp_sampling/plotting.jl
+++ examples/bp_sampling/plotting.jl
@@ -7,7 +7,23 @@ using Statistics
 gr();
 
 FIGS_DIR = "./figs"
-DATA_FILE = "./data/data_*.csv.test"
+DATA_DIR = "./data"
 gid = "CID:$(get_git_id())"
 
-df = DataFrame(CSV.File(DATA_FILE));
+df_en = vcat([DataFrame(CSV.File("$(DATA_DIR)/data_en_$(n).csv.test")) for n=[4,6]]...)
+df_grad = vcat([DataFrame(CSV.File("$(DATA_DIR)/data_grad_$(n).csv.test")) for n=[4,6]]...)
+
+df_res = vcat([DataFrame(CSV.File("$(DATA_DIR)/data_res_$(n).csv.test")) for n=[4,6]]...)
+#transform!(df_res, :energies => x -> eval(Meta.parse(x)) => :energies)
+
+cols_to_eval = [
+    :energies,
+    :max_grads,
+    :opt_pars,
+    :energy_errors,
+    :approx_ratio,
+    :relative_error,
+    :ground_state_overlaps,
+]
+transform!(df_res, cols_to_eval .=> ByRow(x -> eval(Meta.parse(x))) .=> cols_to_eval)
+
