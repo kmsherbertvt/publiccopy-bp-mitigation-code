fb148fe0d55590735d46e5a01bfe2f089f28b594
sam.barron <sam@sambarron.me>
2022-06-20 21:48:44 -0400

    Cleaning up and updating hyperparameters

--- examples/bp_sampling/plotting.jl
+++ examples/bp_sampling/plotting.jl
@@ -7,27 +7,34 @@ using CSV
 using Statistics
 gr();
 
+# Hyperparameters
 FIGS_DIR = "./figs"
 DATA_DIR = "./data"
 DATA_SUFFIX = "csv"
-qubit_range = [4,6]
+qubit_range = 4:2:12
 gid = "CID:$(get_git_id())"
 
+# Data Import
 df_en = vcat([DataFrame(CSV.File("$(DATA_DIR)/data_en_$(n).$(DATA_SUFFIX)")) for n=qubit_range]...)
 df_grad = vcat([DataFrame(CSV.File("$(DATA_DIR)/data_grad_$(n).$(DATA_SUFFIX)")) for n=qubit_range]...)
-
 df_res = vcat([DataFrame(CSV.File("$(DATA_DIR)/data_res_$(n).$(DATA_SUFFIX)")) for n=qubit_range]...)
 cols_to_eval = [ :energies, :max_grads, :opt_pars, :energy_errors, :approx_ratio, :relative_error, :ground_state_overlaps, ]
 transform!(df_res, cols_to_eval .=> ByRow(x -> eval(Meta.parse(x))) .=> cols_to_eval)
 
+# Utility Functions
 function mean_on(df, grp_cols, mean_col; aggr_fn = mean)
     gdf = groupby(df, grp_cols)
     return combine(gdf, mean_col => (x -> aggr_fn(x)) => mean_col)
 end
 
+function fig_out(filename)
+    savefig("$(FIGS_DIR)/$(filename).pdf")
+end
+
+# Main Plots
+
 function plot_1()
-    """ 4 plots horizontally, x axis is number of layers, y axis is var grad, hue num qubits
-    """
+    """ 4 plots, x axis is number of layers, y axis is var grad, hue num qubits """
     plot_names = unique(df_en[!, :method])
 
     plots = Dict()
@@ -41,7 +48,14 @@ function plot_1()
             )
     end
 
-    return plot(collect(values(plots))...)
+    main_plot = plot(collect(values(plots))...)
+    fig_out("variance_convergence")
+    return main_plot
 end
 
-plot_1()
+# Main
+function main()
+    plot_1()
+end
+
+main()
