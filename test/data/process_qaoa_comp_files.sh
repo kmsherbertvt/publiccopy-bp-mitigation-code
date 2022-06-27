b09f85fe1ea122229c5f48b12d3fc6225110699a
sam.barron <sam@sambarron.me>
2022-06-27 14:04:31 -0400

    Adding formatted energy errors

--- test/data/process_qaoa_comp_files.sh
+++ test/data/process_qaoa_comp_files.sh
@@ -1,4 +1,4 @@
 for dir in ./adapt_qaoa_comp/*/; do
     cat $dir/*.py | grep "elist =" | sed "s/elist = //g" > $dir/elist.txt
-    cat $dir/error*.txt | sed "s/[0-9]         //g;s/\n/, /g" > $dir/error_formatted.txt
+    cat $dir/error*.txt | sed -z "s/[0-9]         //g;s/\n/, /g" | sed 's/^/[/;s/$/]/' > $dir/error_formatted.txt
 done
