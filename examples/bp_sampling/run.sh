8181b0e62b0e4dbcb627ba3a47846aaf4cec0cee
sam.barron <sam@sambarron.me>
2022-08-25 12:51:43 -0400

    Adding source for zsh

--- examples/bp_sampling/run.sh
+++ examples/bp_sampling/run.sh
@@ -1,4 +1,4 @@
-julia --project=@. --procs=28 bp_sampling.jl > output_sims.txt
-finished "Simulations"
+source ${ZDOTDIR-$HOME}/.zsh/*.zsh
+
 julia --project=@. plotting.jl > output_plots.txt
 finished "Plots"
