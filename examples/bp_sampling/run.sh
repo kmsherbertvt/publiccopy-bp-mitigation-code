c57562fe8878361269e0362698f79eb87732a848
sam.barron <sam@sambarron.me>
2022-08-25 12:59:22 -0400

    whut

--- examples/bp_sampling/run.sh
+++ examples/bp_sampling/run.sh
@@ -1,4 +1,6 @@
 source ${ZDOTDIR-$HOME}/.zsh/*.zsh
 
+julia --project=@. --procs=28 bp_sampling.jl > output_sims.txt
+finished "Simulations"
 julia --project=@. plotting.jl > output_plots.txt
 finished "Plots"
