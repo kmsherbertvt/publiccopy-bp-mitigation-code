ef9ca6adfba86e47545a9b3b4353044f909b1334
George Barron <george.samuel.barron@gmail.com>
2022-09-06 12:45:03 -0400

    Adding sampling test

--- test/test_sampling.jl
+++ test/test_sampling.jl
@@ -3,6 +3,9 @@ using StatsBase
 using Test
 using Random
 using AdaptBarren
+using Distributions
+
+rng = MersenneTwister(42);
 
 @testset "Test Cost Function and Gradient Sampling" begin
     n = 4
@@ -14,8 +17,15 @@ using AdaptBarren
     hamiltonian = random_regular_max_cut_hamiltonian(n, n-1; weighted = true)
     initial_state = uniform_state(n)
 
-    result_energies, result_gradients = sample_points(hamiltonian, ansatz, initial_state, num_samples)
+    point = rand(rng, Uniform(-pi, +pi), length(ansatz))
 
-    @test length(result_energies) == num_samples
-    @test length(result_gradients) == num_samples * k
+    for _=1:2
+        result_energies, result_gradients = sample_points(hamiltonian, ansatz, initial_state, num_samples; rng=rng, dist=0.0, point=point)
+
+        @test length(result_energies) == num_samples
+        @test length(result_gradients) == num_samples * k
+
+        delta = maximum(result_energies) - minimum(result_energies)
+        @test abs(delta) <= 1e-6
+    end
 end
